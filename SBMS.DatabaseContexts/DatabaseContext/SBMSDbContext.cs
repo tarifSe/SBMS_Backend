6891195972ffa190e15d79888ed3a82de7763f33
tarifSe <gmtariful@gmail.com>
2023-09-21 00:47:59 +0600

    Db context class create and setup for migration create

---
+++ SBMS.DatabaseContexts/DatabaseContext/SBMSDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SBMS.Models.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBMS.DatabaseContexts.DatabaseContext
+{
+    public class SBMSDbContext : DbContext
+    {
+        public SBMSDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
--- SBMSBackend/Program.cs
+++ SBMSBackend/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SBMS.DatabaseContexts.DatabaseContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<SBMSDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("SBMSConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
