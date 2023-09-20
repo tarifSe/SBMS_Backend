91c332da5b523c62c391e6fc3a024f4c59d419d7
tarifSe <gmtariful@gmail.com>
2023-09-21 01:41:17 +0600

    Generic class created

---
+++ SBMS.DAL/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SBMS.DatabaseContexts.DatabaseContext;
+
+namespace SBMS.DAL.Repositories
+{
+    //This is generic class
+    public abstract class Repository<T> where T : class
+    {
+        private readonly SBMSDbContext _context;
+        public Repository(SBMSDbContext sBMSDbContext)
+        {
+            _context = sBMSDbContext;
+        }
+        public DbSet<T> Table { 
+            get 
+            { 
+                return _context.Set<T>();
+            } 
+        }
+
+        public virtual bool Add(T entity)
+        {
+            Table.Add(entity);
+            return _context.SaveChanges() > 0;
+        }
+
+        public virtual bool Update(T entity)
+        {
+            Table.Update(entity);
+            return _context.SaveChanges() > 0;
+        }
+
+        public virtual bool Delete(T entity)
+        {
+            Table.Remove(entity);
+            return _context.SaveChanges() > 0;
+        }
+
+        public virtual T? GetById(int id)
+        {
+            return Table.Find(id);
+        }
+
+        public virtual ICollection<T> GetAll()
+        {
+            return Table.ToList();
+        }
+    }
+}
