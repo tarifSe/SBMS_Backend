03a4cfa1861e3f502c2ff9a6391b0e4debec57a3
tarifSe <gmtariful@gmail.com>
2023-09-23 00:17:47 +0600

    CategoryRepo & CategoryManger created

---
+++ SBMS.BLL/Managers/CategoryManager.cs
@@ -0,0 +1,18 @@
+﻿using SBMS.BLL.Services;
+using SBMS.DAL.Services;
+using SBMS.Models.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBMS.BLL.Managers
+{
+    public class CategoryManager : Manager<Category>, ICategoryManager
+    {
+        public CategoryManager(ICategoryRepository repository) : base(repository)
+        {
+        }
+    }
+}
---
+++ SBMS.DAL/Repositories/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using SBMS.DAL.Services;
+using SBMS.DatabaseContexts.DatabaseContext;
+using SBMS.Models.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBMS.DAL.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(SBMSDbContext sBMSDbContext) : base(sBMSDbContext)
+        {
+
+        }
+    }
+}
