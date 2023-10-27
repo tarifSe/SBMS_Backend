6e5f2ca8654f22d0ef6d129f28d82bca2a35b2d0
tarifSe <gmtariful@gmail.com>
2023-10-27 12:19:46 +0600

    Swawgger UI run IsProduction true

--- SBMSBackend/Program.cs
+++ SBMSBackend/Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddControllersWithViews()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
