6bc06245bf38704dafee4ce337bb43e41b2cfb76
tarifSe <gmtariful@gmail.com>
2024-11-28 18:23:37 +0600

    method override

--- SBMS.DAL/Repositories/PurchaseDetailsRepository.cs
+++ SBMS.DAL/Repositories/PurchaseDetailsRepository.cs
@@ -1,4 +1,5 @@
-﻿using SBMS.DAL.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using SBMS.DAL.Services;
 using SBMS.DatabaseContexts.DatabaseContext;
 using SBMS.Models.EntityModels;
 using System;
@@ -13,6 +14,16 @@ namespace SBMS.DAL.Repositories
     {
         public PurchaseDetailsRepository(SBMSDbContext sBMSDbContext) : base(sBMSDbContext)
         {
+        }
+
+        public override async Task<List<PurchaseDetails>> GetAll()
+        {
+            return await base.Table.Include(c => c.Product).ToListAsync();
+        }
+
+        public override Task<PurchaseDetails?> GetById(int id)
+        {
+            return base.Table.Include(c => c.Product).Where(c => c.Id == id).FirstOrDefaultAsync();
         }
     }
 }
